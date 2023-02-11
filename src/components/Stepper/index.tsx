bf405624966121d3f80c54b4cd86fb20eeb14646
Ismael Rodrigo <86257396+ismael-rodrigo@users.noreply.github.com>
2023-02-10 22:44:19 -0300

    added login user

--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -1,44 +1,54 @@
-import { LoadingOutlined, SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
-import { Col, Row, Steps } from 'antd';
+import { CalendarOutlined, InfoCircleOutlined, LoadingOutlined, SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Col, Input, Row, Space, Steps, Tooltip } from 'antd';
+import LoginStep from './Steps/Login';
 
 const StepperContainer = () => {
 
   
 
   return(
-  <Row>
 
-  <Steps
-    items={[
-      {
-        title: 'Login',
-        status: 'finish',
-        icon: <UserOutlined />,
-      },
-      {
-        title: 'Verification',
-        status: 'finish',
-        icon: <SolutionOutlined />,
-      },
-      {
-        title: 'Pay',
-        status: 'process',
-        icon: <LoadingOutlined />,
-      },
-      {
-        title: 'Done',
-        status: 'wait',
-        icon: <SmileOutlined />,
-      },
-    ]}
-  />
+    <Col span={12} offset={6}>
 
-    <Row>
+        <Steps
+          items={[
+            {
+              title: 'Login',
+              status: 'finish',
+              icon: <UserOutlined />,
+            },
+            {
+              title: 'Serviços',
+              status: 'finish',
+              icon: <SolutionOutlined />,
+            },
+            {
+              title: 'Horários',
+              status: 'process',
+              icon: <LoadingOutlined />,
+            },
+            {
+              title: 'Confirmação',
+              status: 'wait',
+              icon: <SmileOutlined />,
+            },
+            {
+              title: 'Concluído',
+              status: 'wait',
+              icon: <SmileOutlined />,
+            },
+          ]}
+        />
 
-        <h1>HELLO CONTAINER 1</h1>
+      <br />
+      <br />
 
-    </Row>
-  </Row>
+      <Row>
+        <LoginStep/>
+      </Row>
+      
+    </Col>
+
 
 )}
 
