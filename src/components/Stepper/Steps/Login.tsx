7cbb19a856e2490ed2c6c2a191cba7aaaee890db
Ismael Rodrigo <86257396+ismael-rodrigo@users.noreply.github.com>
2023-02-21 23:42:43 -0300

    intial login with cpf and password

--- src/components/Stepper/Steps/Login.tsx
+++ src/components/Stepper/Steps/Login.tsx
@@ -1,15 +1,36 @@
-import { CalendarOutlined, InfoCircleOutlined, UserOutlined } from "@ant-design/icons";
+import { EyeInvisibleOutlined, EyeTwoTone, InfoCircleOutlined, UnlockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Input, Row, Space, Tooltip } from "antd";
-import { useContext } from "react";
+import { useContext , useState } from "react";
 import { ScheduleContext } from "../../../context/NewScheduleContext";
+import { cpf as cpfValidator } from 'cpf-cnpj-validator'
 
 export default function LoginStep(){
     const handler = useContext(ScheduleContext)
+    const [showPassord ,setShowPassord ] = useState(false)
+    const [cpf,setCpf] = useState('')
+    const [cpfError, setCpfError] = useState(false)
+
+    const checkIfUserAlreadyExists = (cpf:string)=>{
+      // verify user in backend
+        // if user already exist, show password input
+        // if not exists, open create user page
+    }
+
+    const handlerCpfInput = (value:string) =>{
+      setCpf(value)
+      if(value.length < 11) return setCpfError(false)
+      if(!cpfValidator.isValid(value)) return setCpfError(true)
+      setCpfError(false)
+    }
+
 
     return(
       <Space direction={'vertical'} size={15} style={{ width:'100%' }}>
         <Row>
           <Input
+            maxLength={11}
+            value={cpf}
+            status={cpfError ? 'error' : undefined}
             size='large'
             placeholder=" Insira seu CPF"
             prefix={<UserOutlined className="site-form-item-icon" />}
@@ -18,22 +39,23 @@ export default function LoginStep(){
                 <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
               </Tooltip>
             }
+            onChange={(v)=>{
+              handlerCpfInput(v.target.value)
+            }}
+
             />
         </Row>
         <Row>
-        <Input 
-            size='large'
-            placeholder=" Insira sua data de nascimento"
-            prefix={<CalendarOutlined />}
-            suffix={
-              <Tooltip title="Data de nascimento dia/mes/ano">
-                <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-              </Tooltip>
-            }
-            />
+        {showPassord &&
+        <Input.Password
+        size='large'
+        placeholder="Senha de acesso"
+        prefix={<UnlockOutlined  className="site-form-item-icon" />}
+        iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+        />}
         </Row>
             
-        <Button type="primary" block size='large' style={{marginTop:10}} onClick={()=>handler?.setPage({...handler.page, login:'finish' ,register_user:'process'})} >
+        <Button disabled={cpfError || cpf.length < 11} type="primary" block size='large' style={{marginTop:10}} onClick={()=>handler?.setPage({...handler.page, login:'finish' ,register_user:'process'})} >
           Iniciar agendamento
         </Button>
         <Button type="link" size='small'>
